b30889a0404e92e451484fa4230ff6de599971e8
Zenul <126080810+Zenul00@users.noreply.github.com>
2023-06-26 15:57:52 +0530

    Create Q4_canPlaceFlowers.java

---
+++ AS_2/Q4_canPlaceFlowers.java
@@ -0,0 +1,40 @@
+/*Question 4
+You have a long flowerbed in which some of the plots are planted, and some are not.
+However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 
+means not empty, and an integer n, return true if n new flowers can be planted in the 
+flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true */
+
+package PPT.AS_2;
+
+public class Q4_canPlaceFlowers {
+
+    static boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        if(n == 0){
+            return true;
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < flowerbed.length; i++) {
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && 
+            (i == flowerbed.length - 1 || flowerbed[i+1] == 0)) {
+                count++;
+            }
+        }
+        if(count == n) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int [] flowerbed = {1,0,0,0,1};
+        int n = 2;
+        System.out.println(canPlaceFlowers(flowerbed, n));
+    }
+}
