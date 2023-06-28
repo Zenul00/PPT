49dabad87780a74cbcf60616a52b7bae357e3065
Zenul <126080810+Zenul00@users.noreply.github.com>
2023-06-28 23:34:26 +0530

    Create  Q8_canAttendAllMeetings.java

---
+++ AS_3/Q8_canAttendAllMeetings.java
@@ -0,0 +1,22 @@
+/*Question 8
+Given an array of meeting time intervals where intervals[i] = [starti, endi],
+determine if a person could attend all meetings.
+
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false */
+
+package PPT.AS_3;
+
+public class Q8_canAttendAllMeetings {
+    
+    static boolean canAttendMeetings(int[][] intervals) {
+
+        // not solve yet
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
