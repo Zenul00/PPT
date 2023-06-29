f1baffd01e268b9a41b024ebf0eafe6be4739d43
Zenul <126080810+Zenul00@users.noreply.github.com>
2023-06-29 17:41:54 +0530

    Create Q3_squaresOfSortedArray.java

---
+++ AS_5/Q3_squaresOfSortedArray.java
@@ -0,0 +1,51 @@
+/*<aside>
+💡 **Question 6**
+Given an integer array nums sorted in **non-decreasing** order, return *an array of **the squares of each number** sorted in non-decreasing order*.
+
+**Example 1:**
+
+Input: nums = [-4,-1,0,3,10]
+
+Output: [0,1,9,16,100]
+
+**Explanation:** After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100]
+
+</aside> */
+/*<aside>
+💡 **Question 6**
+Given an integer array nums sorted in **non-decreasing** order, return *an array of **the squares of each number** sorted in non-decreasing order*.
+
+**Example 1:**
+
+Input: nums = [-4,-1,0,3,10]
+
+Output: [0,1,9,16,100]
+
+**Explanation:** After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100]
+
+</aside> */
+
+package PPT.AS_5;
+
+import java.util.Arrays;
+
+public class Q3_squaresOfSortedArray {
+
+    static int[] sortedSquares(int[] A) {
+
+        for (int i = 0; i < A.length; i++){
+            A[i] = A[i] * A[i];
+        }
+            Arrays.sort(A);
+            return A;
+    }
+
+    public static void main(String[] args) {
+
+        int [] nums = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(sortedSquares(nums)));
+    }
+    
+}
