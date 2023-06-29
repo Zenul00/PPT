e58874cc9318acfab89659cf41f0250f02e3be37
Zenul <126080810+Zenul00@users.noreply.github.com>
2023-06-29 17:33:42 +0530

    Update Q8_canAttendAllMeetings.java

--- AS_3/Q8_canAttendAllMeetings.java
+++ AS_3/Q8_canAttendAllMeetings.java
@@ -12,10 +12,25 @@ public class Q8_canAttendAllMeetings {
     
     static boolean canAttendMeetings(int[][] intervals) {
 
-        // not solve yet
+        int len = intervals.length;
+        int[] startTime = new int[len];
+        int[] endTime = new int[len];
+        int count = 0;
+        for(int[] interval: intervals){
+            startTime[count] = interval[0];
+            endTime[count++] = interval[1];
+        }
+        Arrays.sort(startTime);
+        Arrays.sort(endTime);
+        for(int i = 1; i < len; i++){
+            if(startTime[i] < endTime[i - 1]) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
+        int [][] intervals = {{0,30},{5,10},{15,20}};
+        System.out.println(canAttendMeetings(intervals));
         
     }
     
